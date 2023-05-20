d766af57bf05aea4c45511feb6244118334b56ca
merallamar <134140938+merallamar@users.noreply.github.com>
2023-05-20 14:23:21 +0300

    Add files via upload

---
+++ website/data.js
@@ -0,0 +1,36 @@
+export let plansList = [
+	{
+		//####################################### start updating as needed #######################################
+		name: "normal ",
+		monthlySubPrice: 80,
+		yearlySubPrice: 900,
+		Storagbody: 300,
+		andpetite: 400,
+		strongbody: false,
+		andpetite: false,
+		customerCare: false
+	},
+	{
+		name: "pro",
+		monthlySubPrice: 150,
+		yearlySubPrice: 1500,
+		strongbody:500,
+		andpetite: 600,
+		strongbody: true,
+		andpetite: false,
+		customerCare: "By Email"
+	},
+	{
+		name: "premium",
+		monthlySubPrice: 200,
+		yearlySubPrice: 2200,
+		strongbody: 800,
+		andpetite: 1000,
+		strongbody: true,
+		andpetite: true,
+		customerCare: "24/7 Line"
+		//#######################################      stop  updating      #######################################
+		                                        
+	}
+]
+
